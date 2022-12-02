c60fe89320a031bc18a875dc4947e4d7b7089344
tran0460 <tran0460@algonquinlive.com>
2022-12-02 15:05:54 -0500

    create app context

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import { styles } from "../Styles";
