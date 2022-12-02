d8778fa28ca79a26b6ff166ed8665d6da810b55b
Hygor Costa <cost0142@algonquinlive.com>
2022-12-02 18:39:38 -0500

    pass data in context

--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -3,13 +3,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 import { createSearchQuery, getData } from "../api.services";
 import { styles } from "../Styles";
+import { useAppContext } from "../AppContext";
+// , set data using setSearchResults in DiscoverScreen.js
 
 const SearchScreen = () => {
+  const { searchResults, setSearchResults } = useAppContext();
   const [searchTerm, setSearchTerm] = useState("");
   const onSearch = async () => {
     const queries = createSearchQuery({ term: searchTerm });
     const data = await getData(queries);
-    console.log(data);
+    setSearchResults(data);
   };
   return (
     <SafeAreaView style={styles.container}>
