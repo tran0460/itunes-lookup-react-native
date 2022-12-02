a6fdf8f0384ea3be91825f0d02867ec2a04d072a
Hygor Costa <cost0142@algonquinlive.com>
2022-12-02 15:11:25 -0500

    Created Text Input  and Search button

--- Styles.js
+++ Styles.js
@@ -4,4 +4,11 @@ export const styles = StyleSheet.create({
   screenContainer: {
     paddingHorizontal: 16,
   },
+  searchInput: {
+    height: 40,
+    borderColor: "#CCCCCC",
+    borderWidth: 1,
+    paddingLeft: 15,
+    margin: 10,
+  },
 });
