c612dc098de787a048433db17c0e2f9e9c321d7e
tran0460 <tran0460@algonquinlive.com>
2022-12-02 10:44:56 -0500

    basic setup

--- App.js
+++ App.js
@@ -3,15 +3,20 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen";
+import DiscoverScreen from "./screens/DiscoverScreen";
 
+/**
+ * react-native-safe-area-context
+ */
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="" />
-        <Tab.Screen name="" />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Discover" component={DiscoverScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
