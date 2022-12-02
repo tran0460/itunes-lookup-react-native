63a307cbedcf87c5aff57bda0620d10f330a43c8
tran0460 <tran0460@algonquinlive.com>
2022-12-02 10:32:32 -0500

    create bottom navigation

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="" />
+        <Tab.Screen name="" />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
