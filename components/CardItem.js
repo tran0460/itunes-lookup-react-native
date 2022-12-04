89eba9790db7465f6aa42153e56ef720e3cc0ea2
tran0460 <tran0460@algonquinlive.com>
2022-12-03 20:25:36 -0500

    styling

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,12 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
-const CardItem = () => {
+const CardItem = ({ data, type }) => {
+  console.log(data);
+  const upScaledImage = data.artworkUrl100.replace("100x100bb", "400x400bb");
   return (
-    <View>
-      <Text>CardItem</Text>
+    <View style={styles.cardContainer}>
+      <Image
+        style={{ width: "100%", height: 200 }}
+        source={{ uri: upScaledImage }}
+      />
+      <Text style={styles.cardTitle}>{data.collectionName}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    flexDirection: "column",
+    maxWidth: 200,
+  },
+});
 
 export default CardItem;
