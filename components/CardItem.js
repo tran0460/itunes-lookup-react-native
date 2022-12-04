26a8ab3add0e560a7eeaec517f26c73b9462a3fd
tran0460 <tran0460@algonquinlive.com>
2022-12-03 20:10:15 -0500

    create components

---
+++ components/CardItem.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CardItem = () => {
+  return (
+    <View>
+      <Text>CardItem</Text>
+    </View>
+  );
+};
+
+export default CardItem;
