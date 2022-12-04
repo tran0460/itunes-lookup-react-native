4ed5dace7c5aebfa4f5569874a5825dfc89d6d92
tran0460 <tran0460@algonquinlive.com>
2022-12-03 21:14:06 -0500

    create card styling for movies card

--- components/CardItem.js
+++ components/CardItem.js
@@ -2,27 +2,58 @@ import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
 const CardItem = ({ data, type }) => {
-  const upScaledImage = data.artworkUrl100.replace("100x100bb", "400x400bb");
-  return (
-    <View style={styles.cardContainer}>
-      <Image style={styles.cardImage} source={{ uri: upScaledImage }} />
-      <Text style={styles.cardTitle}>{data.collectionName}</Text>
-    </View>
-  );
+  const upScaledImage400 = data.artworkUrl100.replace("100x100bb", "400x400bb");
+  const upScaledImage600 = data.artworkUrl100.replace("100x100bb", "600x600bb");
+  const upScaledImage800 = data.artworkUrl100.replace("100x100bb", "800x800bb");
+
+  console.log(data);
+  switch (type) {
+    case "album":
+      return (
+        <View style={[styles.cardContainer, styles.albumCard]}>
+          <Image
+            style={[styles.cardImage, styles.albumImage]}
+            source={{ uri: upScaledImage400 }}
+          />
+          <Text style={styles.cardTitle}>{data.collectionName}</Text>
+        </View>
+      );
+    case "movie":
+      return (
+        <View style={[styles.cardContainer, styles.movieCard]}>
+          <Image
+            resizeMode="fill"
+            style={[styles.cardImage, styles.movieImage]}
+            source={{ uri: upScaledImage400 }}
+          />
+          <Text style={styles.cardTitle}>{data.trackName}</Text>
+        </View>
+      );
+  }
 };
 
 const styles = StyleSheet.create({
   cardContainer: {
     flex: 1,
     flexDirection: "column",
-    maxWidth: 200,
     marginRight: 20,
+  },
+  albumCard: {
+    width: 200,
+  },
+  movieCard: {
+    width: 200,
   },
   cardImage: {
     width: "100%",
-    height: 200,
     borderRadius: 4,
     marginBottom: 12,
+  },
+  albumImage: {
+    height: 200,
+  },
+  movieImage: {
+    height: 300,
   },
 });
 
