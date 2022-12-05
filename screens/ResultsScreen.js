4a2d3f91ecfc1060bf048fc9efa23b12a2794194
Hygor Costa <cost0142@algonquinlive.com>
2022-12-04 19:12:54 -0500

    render itens on flat list

--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
+import { styles } from "../Styles";
+import { useAppContext } from "../AppContext";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
+import { FlatList } from "react-native-gesture-handler";
 
 const ResultsScreen = () => {
+  const { searchResults } = useAppContext();
+  function renderItem({ item }) {
+    return <Text>{item.trackName}</Text>;
+  }
+
   return (
     <SafeAreaView>
+      <FlatList data={searchResults} renderItem={renderItem} />
       <Text>ResultsScreen</Text>
     </SafeAreaView>
   );
