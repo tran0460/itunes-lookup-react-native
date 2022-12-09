932787a65e2f111845b29bf3580f985127622ff1
Hygor Costa <cost0142@algonquinlive.com>
2022-12-09 15:27:15 -0500

    add podcast path

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -24,30 +24,47 @@ const DetailsScreen = () => {
         />
 
         <Text style={styles.textdetails}>{currentItem.artistName}</Text>
-        <Text style={styles.textdetails}>{currentItem.trackName}</Text>
-        <Text>{currentItem.collectionCensoredName}</Text>
-      </View>
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity
-          style={[styles.preview, styles.shadow]}
-          onPress={() => Linking.openURL(currentItem.previewUrl)}
-        >
-          <Text style={styles.buttonText}> Open preview in browser </Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.preview}
-          onPress={() => {
-            if (Platform.OS === "android")
-              return Alert.alert("Not supported on Android");
-            Linking.openURL(
-              `music${currentItem.trackViewUrl.replace("https", "")}`
-            );
-          }}
-        >
-          <Text style={styles.buttonText}> Open in Apple Music </Text>
-        </TouchableOpacity>
+        <Text style={styles.textdetails}>
+          {currentItem.collectionType === "Album"
+            ? currentItem.collectionName
+            : currentItem.trackName}
+        </Text>
+
+        <Text>${currentItem.collectionPrice}</Text>
       </View>
+      {currentItem.collectionType === "Album" ? null : (
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity
+            style={[styles.preview, styles.shadow]}
+            onPress={() => Linking.openURL(currentItem.previewUrl)}
+          >
+            <Text style={styles.buttonText}> Open preview in browser </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.preview}
+            onPress={() => {
+              if (Platform.OS === "android")
+                return Alert.alert("Not supported on Android");
+              if (currentItem.kind === "podcast") {
+                return Linking.openURL(
+                  `podcast${currentItem.feedUrl.replace("https", "")}`
+                );
+              }
+              return Linking.openURL(
+                `music${currentItem.trackViewUrl.replace("https", "")}`
+              );
+            }}
+          >
+            <Text style={styles.buttonText}>
+              {currentItem.kind === "podcast"
+                ? "Open in Podcasts"
+                : " Open in Apple Music "}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
