d0c8f23514e04d917eff09356f6c08fe7cd1e8e3
Hygor Costa <cost0142@algonquinlive.com>
2022-12-09 16:12:53 -0500

    add navigation from cards items

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,35 +1,47 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { useAppContext } from "../AppContext";
 import { styles } from "../Styles";
 import React from "react";
 
-const CardItem = ({ data, type }) => {
+const CardItem = ({ data, type, navigation }) => {
   const upScaledImage400 = data.artworkUrl100.replace("100x100bb", "400x400bb");
   const upScaledImage600 = data.artworkUrl100.replace("100x100bb", "600x600bb");
   const upScaledImage800 = data.artworkUrl100.replace("100x100bb", "800x800bb");
-  switch (type) {
-    case "album":
-      return (
-        <View style={[styles.cardContainer, styles.albumCard]}>
-          <Image
-            style={[styles.cardImage, styles.albumImage]}
-            source={{ uri: upScaledImage400 }}
-          />
-          <Text lineBreakMode="tail" style={[styles.albumTitle]}>
-            {data.collectionName}
-          </Text>
-        </View>
-      );
-    case "movie":
-      return (
-        <View style={[styles.cardContainer, styles.movieCard]}>
-          <Image
-            resizeMode="fill"
-            style={[styles.cardImage, styles.movieImage]}
-            source={{ uri: upScaledImage400 }}
-          />
-          <Text style={[styles.movieTitle]}>{data.trackName}</Text>
-        </View>
-      );
+  const { setCurrentItem } = useAppContext();
+  const navigate = () => {
+    setCurrentItem(data);
+    navigation.navigate("DetailsScreen");
+  };
+  {
+    switch (type) {
+      case "album":
+        return (
+          <TouchableOpacity onPress={navigate}>
+            <View style={[styles.cardContainer, styles.albumCard]}>
+              <Image
+                style={[styles.cardImage, styles.albumImage]}
+                source={{ uri: upScaledImage400 }}
+              />
+              <Text lineBreakMode="tail" style={[styles.albumTitle]}>
+                {data.collectionName}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        );
+      case "movie":
+        return (
+          <TouchableOpacity onPress={navigate}>
+            <View style={[styles.cardContainer, styles.movieCard]}>
+              <Image
+                resizeMode="fill"
+                style={[styles.cardImage, styles.movieImage]}
+                source={{ uri: upScaledImage400 }}
+              />
+              <Text style={[styles.movieTitle]}>{data.trackName}</Text>
+            </View>
+          </TouchableOpacity>
+        );
+    }
   }
 };
 
